51cb6fd16d9012cf4dcab16d51f694b9a7c815e0
theevik <viktorerko2000@gmail.com>
2021-06-15 16:22:06 +0200

    fsad

--- Two Button Game Latest/Assets/Scripts/subwaytalk.cs
+++ Two Button Game Latest/Assets/Scripts/subwaytalk.cs
@@ -7,6 +7,7 @@ using UnityEngine.SceneManagement;
 public class subwaytalk : MonoBehaviour
 {
     public GameObject subs1;
+    public GameObject subs2;
 
     public GameObject aero, beta, frost, naiden, weazel;
 
@@ -23,15 +24,18 @@ public class subwaytalk : MonoBehaviour
         weazel.SetActive(false);
 
         subs1.GetComponent<Text>().text = "Weten jullie zeker dat we Weazel kunnen vetrouwen, dit ziet er... verdacht uit.";
+        subs2.GetComponent<Text>().text = "Boys are you sure we can trust Weazel, all of this seems kind of… sketchy.";
         yield return new WaitForSeconds(4f);
 
+       
         aero.SetActive(true);
         beta.SetActive(false);
         frost.SetActive(false);
         naiden.SetActive(false);
         weazel.SetActive(false);
-        subs1.GetComponent<Text>().text = "He’s Just your average scumbag who spends most his day at the bar, nothing we should be too worried about. Ach, hij is gewoon een klootzak die de meeste van zijn tijd in de bar besteed, niks om je zorgen over te maken.";
-        yield return new WaitForSeconds(2f);
+        subs1.GetComponent<Text>().text = " Ach, hij is gewoon een klootzak die de meeste van zijn tijd in de bar besteed, niks om je zorgen over te maken.";
+        subs2.GetComponent<Text>().text = "He’s Just your average scumbag who spends most his day at the bar, nothing we should be too worried about. ";
+        yield return new WaitForSeconds(6f);
 
         aero.SetActive(false);
         beta.SetActive(true);
@@ -39,6 +43,7 @@ public class subwaytalk : MonoBehaviour
         naiden.SetActive(false);
         weazel.SetActive(false);
         subs1.GetComponent<Text>().text = "Hoe kennen jullie elkaar eigenlijk?";
+        subs2.GetComponent<Text>().text = "How do you two know each other anyway?";
         yield return new WaitForSeconds(2f);
 
         aero.SetActive(true);
@@ -47,6 +52,16 @@ public class subwaytalk : MonoBehaviour
         naiden.SetActive(false);
         weazel.SetActive(false);
         subs1.GetComponent<Text>().text = "Ik besteedde vroeger veel tijd in de bar. ";
+        subs2.GetComponent<Text>().text = "I used to spend a lot of time at the bar myself. ";
+        yield return new WaitForSeconds(2.2f);
+
+        aero.SetActive(false);
+        beta.SetActive(true);
+        frost.SetActive(false);
+        naiden.SetActive(false);
+        weazel.SetActive(false);
+        subs1.GetComponent<Text>().text = "Mooi ";
+        subs2.GetComponent<Text>().text = "Classy.";
         yield return new WaitForSeconds(2f);
 
 
@@ -56,7 +71,8 @@ public class subwaytalk : MonoBehaviour
         naiden.SetActive(false);
         weazel.SetActive(true);
         subs1.GetComponent<Text>().text = "HEY! Wat doen jullie hier? Ren maar snel weg nu het nog kan";
-        yield return new WaitForSeconds(2f);
+        subs2.GetComponent<Text>().text = "HEY! What are you bastards doing here, get lost while you still have the chance.";
+        yield return new WaitForSeconds(4.2f);
 
 
         aero.SetActive(false);
@@ -65,6 +81,7 @@ public class subwaytalk : MonoBehaviour
         naiden.SetActive(true);
         weazel.SetActive(false);
         subs1.GetComponent<Text>().text = "Oh ja? Laat ons maar eens waar de uitgang is, eikel! ";
+        subs2.GetComponent<Text>().text = "Oh yea? Well how about you come show me where the exit is punk. ";
         yield return new WaitForSeconds(2f);
 
 
--- Two Button Game Latest/Assets/Scripts/weazeldefeatedtalk.cs
+++ Two Button Game Latest/Assets/Scripts/weazeldefeatedtalk.cs
@@ -6,6 +6,7 @@ using UnityEngine.SceneManagement;
 public class weazeldefeatedtalk : MonoBehaviour
 {
     public GameObject subs1;
+    public GameObject subs2;
 
     public GameObject aero, beta, frost, naiden, weazel;
 
@@ -22,6 +23,7 @@ public class weazeldefeatedtalk : MonoBehaviour
         weazel.SetActive(false);
 
         subs1.GetComponent<Text>().text = "Genoeg gespeeld, tijd om wat vragen te beantwoorden.";
+        subs2.GetComponent<Text>().text = "Playtime is over pal, time to answer some questions.";
         yield return new WaitForSeconds(3f);
 
         aero.SetActive(false);
@@ -30,6 +32,7 @@ public class weazeldefeatedtalk : MonoBehaviour
         naiden.SetActive(false);
         weazel.SetActive(true);
         subs1.GetComponent<Text>().text = "Ik ken jou… Jij werkt voor die eikel Gimbo!";
+        subs2.GetComponent<Text>().text = "I know you… You work for that prick Gimbo.";
         yield return new WaitForSeconds(2.8f);
 
         aero.SetActive(false);
@@ -38,6 +41,7 @@ public class weazeldefeatedtalk : MonoBehaviour
         naiden.SetActive(false);
         weazel.SetActive(false);
         subs1.GetComponent<Text>().text = "Hey, houd de baas zijn naam uit je mond!!";
+        subs2.GetComponent<Text>().text = "Hey, you better keep the boss’s name out your mouth.   ";
         yield return new WaitForSeconds(2.7f);
 
         aero.SetActive(false);
@@ -46,6 +50,7 @@ public class weazeldefeatedtalk : MonoBehaviour
         naiden.SetActive(false);
         weazel.SetActive(true);
         subs1.GetComponent<Text>().text = "Wat moeten jullie met mij, zijn jullie niet op zoek naar hem?";
+        subs2.GetComponent<Text>().text = "What business do you have with me, aren’t you busy looking for him.";
         yield return new WaitForSeconds(3.2f);
 
 
@@ -55,6 +60,7 @@ public class weazeldefeatedtalk : MonoBehaviour
         naiden.SetActive(true);
         weazel.SetActive(false);
         subs1.GetComponent<Text>().text = "Luister, vertel ons wat je weet over Gimbo’s verdwijning.";
+        subs2.GetComponent<Text>().text = "Listen man, just tell us what you know about Gimbo's disappearance. ";
         yield return new WaitForSeconds(3.2f);
 
 
@@ -64,6 +70,7 @@ public class weazeldefeatedtalk : MonoBehaviour
         naiden.SetActive(false);
         weazel.SetActive(true);
         subs1.GetComponent<Text>().text = "En waarom denk je dat ik daar iets over zou zeggen? Wat heb ik eraan?";
+        subs2.GetComponent<Text>().text = "And why do you think I will open my mouth for you. What’s in it for me?";
         yield return new WaitForSeconds(3.8f);
 
 
@@ -73,6 +80,7 @@ public class weazeldefeatedtalk : MonoBehaviour
         naiden.SetActive(false);
         weazel.SetActive(false);
         subs1.GetComponent<Text>().text = "Dan mag je je andere hand houden... ";
+        subs2.GetComponent<Text>().text = "You get to keep your other hand. ";
         yield return new WaitForSeconds(2.5f);
 
 
@@ -82,6 +90,7 @@ public class weazeldefeatedtalk : MonoBehaviour
         naiden.SetActive(false);
         weazel.SetActive(true);
         subs1.GetComponent<Text>().text = "Kijk, ik heb geen idee wie hier bij betrokken is, veel mensen will Gimbo vermoorden in deze stad.";
+        subs2.GetComponent<Text>().text = "Look, I don’t know who’s involved in this, a lot of people in this city want Gimbo dead. ";
         yield return new WaitForSeconds(5.3f);
 
 
@@ -91,6 +100,7 @@ public class weazeldefeatedtalk : MonoBehaviour
         naiden.SetActive(false);
         weazel.SetActive(true);
         subs1.GetComponent<Text>().text = "Hoe weten jullie zo zeker dat hij niet al aan het rotten is in een goot?";
+        subs2.GetComponent<Text>().text = "How do you know he’s not rotting in some ditch already?";
         yield return new WaitForSeconds(4.5f);
 
 
@@ -100,6 +110,7 @@ public class weazeldefeatedtalk : MonoBehaviour
         naiden.SetActive(false);
         weazel.SetActive(false);
         subs1.GetComponent<Text>().text = "Jij stelt hier de vragen niet. ";
+        subs2.GetComponent<Text>().text = "You don’t get to ask questions. ";
         yield return new WaitForSeconds(2f);
 
         
@@ -109,6 +120,7 @@ public class weazeldefeatedtalk : MonoBehaviour
         naiden.SetActive(false);
         weazel.SetActive(false);
         subs1.GetComponent<Text>().text = "Kom op Weazel, jij en je maatjes zijn altijd aan het rondhangen in de bar, je moet toch wel iets gehoord hebben. ";
+        subs2.GetComponent<Text>().text = "Come on Weazel, you and your buddies hang out at the bar all day, you must have heard something suspicious.";
         yield return new WaitForSeconds(6f);
 
 
@@ -118,6 +130,7 @@ public class weazeldefeatedtalk : MonoBehaviour
         naiden.SetActive(false);
         weazel.SetActive(true);
         subs1.GetComponent<Text>().text = "Nu je het zegt, een van Batka’s mannen hadden het over een “grote bezorging” ergens later vanavond.";
+        subs2.GetComponent<Text>().text = "Now that you mention it, I remember last night some of Batka’s men were going on about a big delivery that’s happening later tonight.";
         yield return new WaitForSeconds(6f);
 
         aero.SetActive(false);
@@ -126,6 +139,7 @@ public class weazeldefeatedtalk : MonoBehaviour
         naiden.SetActive(false);
         weazel.SetActive(false);
         subs1.GetComponent<Text>().text = "Wat voor bezorging?";
+        subs2.GetComponent<Text>().text = "What type of delivery?";
         yield return new WaitForSeconds(2.5f);
 
 
@@ -135,6 +149,7 @@ public class weazeldefeatedtalk : MonoBehaviour
         naiden.SetActive(false);
         weazel.SetActive(true);
         subs1.GetComponent<Text>().text = "Eerst dacht ik dat het weer over die drugs ging die ze aan het maken zijn, maar een m’n maten...";
+        subs2.GetComponent<Text>().text = "At first, I thought it was more of those drugs they’ve been making, but one of the boys told... ";
         yield return new WaitForSeconds(5f);
 
         aero.SetActive(false);
@@ -143,6 +158,7 @@ public class weazeldefeatedtalk : MonoBehaviour
         naiden.SetActive(false);
         weazel.SetActive(true);
         subs1.GetComponent<Text>().text = "...vertelde me dan hij er meerdere zag rondhangen in een verlaten metrostation niet ver van hier.";
+        subs2.GetComponent<Text>().text = "...me he saw a bunch of them hanging around the abandoned subway station not far from here.";
         yield return new WaitForSeconds(5f);
 
         aero.SetActive(false);
@@ -151,6 +167,7 @@ public class weazeldefeatedtalk : MonoBehaviour
         naiden.SetActive(false);
         weazel.SetActive(true);
         subs1.GetComponent<Text>().text = "Lijkt erop dat ze een trein gebruiken, rare drugs bezorging als je het mij vraagt.";
+        subs2.GetComponent<Text>().text = "Seems like they’ll be using a train this time, pretty irregular for a drug delivery if you ask me.";
         yield return new WaitForSeconds(4f);
 
         aero.SetActive(true);
@@ -159,6 +176,7 @@ public class weazeldefeatedtalk : MonoBehaviour
         naiden.SetActive(false);
         weazel.SetActive(false);
         subs1.GetComponent<Text>().text = "Klinkt alsof het het waar is om een kijkje te nemen, wat zeggen jullie?";
+        subs2.GetComponent<Text>().text = "What do you say guys, sounds like it’s worth checking out.";
         yield return new WaitForSeconds(3.5f);
 
         aero.SetActive(false);
@@ -167,6 +185,7 @@ public class weazeldefeatedtalk : MonoBehaviour
         naiden.SetActive(true);
         weazel.SetActive(false);
         subs1.GetComponent<Text>().text = "Tis beter dan niks, ergste wat er kan gebeuren is dat we meer informatie uit hun slaan.";
+        subs2.GetComponent<Text>().text = "Better than nothing, worst case scenario we beat more info out of them.";
         yield return new WaitForSeconds(4.2f);
 
         aero.SetActive(false);
@@ -175,6 +194,7 @@ public class weazeldefeatedtalk : MonoBehaviour
         naiden.SetActive(false);
         weazel.SetActive(false);
         subs1.GetComponent<Text>().text = "Ik weet waar het is, volg mij. ";
+        subs2.GetComponent<Text>().text = ": I know the place, let’s go.";
         yield return new WaitForSeconds(3.5f);
 
         aero.SetActive(false);
@@ -183,6 +203,7 @@ public class weazeldefeatedtalk : MonoBehaviour
         naiden.SetActive(false);
         weazel.SetActive(true);
         subs1.GetComponent<Text>().text = "Wat dachten jullie van wat financiele compensatie, voor de informatie weetjewel?";
+        subs2.GetComponent<Text>().text = "How about a financial compensation…for my troubles.";
         yield return new WaitForSeconds(4.5f);
 
         aero.SetActive(false);
@@ -191,6 +212,7 @@ public class weazeldefeatedtalk : MonoBehaviour
         naiden.SetActive(false);
         weazel.SetActive(false);
         subs1.GetComponent<Text>().text = "Ik dacht dat we het al eens  waren dat je je arm mocht houden.";
+        subs2.GetComponent<Text>().text = "I thought we already settled on you keeping your arm.";
         yield return new WaitForSeconds(4.5f);
 
         aero.SetActive(false);
@@ -199,7 +221,8 @@ public class weazeldefeatedtalk : MonoBehaviour
         naiden.SetActive(false);
         weazel.SetActive(true);
         subs1.GetComponent<Text>().text = "Klootzakken…";
-        
+        subs2.GetComponent<Text>().text = "You assholes…";
+
 
     }
 
