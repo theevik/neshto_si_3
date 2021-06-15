386d714e705b216be3829d048d59ff7760993200
theevik <viktorerko2000@gmail.com>
2021-06-15 19:50:20 +0200

    fsafsa

--- Two Button Game Latest/Assets/Scripts/talk4_2.cs
+++ Two Button Game Latest/Assets/Scripts/talk4_2.cs
@@ -7,6 +7,7 @@ using UnityEngine.SceneManagement;
 public class talk4_2 : MonoBehaviour
 {
     public GameObject subs1;
+    public GameObject subs2;
 
     public GameObject aero, beta, frost, naiden, weazel;
 
@@ -23,23 +24,37 @@ public class talk4_2 : MonoBehaviour
         weazel.SetActive(false);
 
         subs1.GetComponent<Text>().text = "Ik vraag me af hoe Batka een trein hebben gekregen zo ver aan het randje van de stad.";
-        yield return new WaitForSeconds(2f);
+        subs2.GetComponent<Text>().text = "I wonder how Batka got his hands on a train here in the outer region of the city. ";
+        yield return new WaitForSeconds(6f);
 
         aero.SetActive(false);
         beta.SetActive(true);
         frost.SetActive(false);
         naiden.SetActive(false);
         weazel.SetActive(false);
-        subs1.GetComponent<Text>().text = "Hij heft vast een kapotte gefixed die achtergelaten was wanneer de stad in drieën was gesplitst. Meeste ervan waren verplaatst naar de binnen en centrale gebieden voordat reizen ernaar verboden werd.";
-        yield return new WaitForSeconds(2f);
+        subs1.GetComponent<Text>().text = "Hij heft vast een kapotte gefixed die achtergelaten was wanneer de stad in drieën was gesplitst.";
+        subs2.GetComponent<Text>().text = "He must’ve managed to fix a broken one that was left behind when the city split in three.";
+        yield return new WaitForSeconds(5f);
+
+        aero.SetActive(false);
+        beta.SetActive(true);
+        frost.SetActive(false);
+        naiden.SetActive(false);
+        weazel.SetActive(false);
+        subs1.GetComponent<Text>().text = "Meeste ervan waren verplaatst naar de binnen en centrale gebieden voordat reizen ernaar verboden werd.";
+        subs2.GetComponent<Text>().text = "Most of the trains got moved to the inner and central regions before travel to them became restricted. ";
+        yield return new WaitForSeconds(7f);
+
 
         aero.SetActive(false);
         beta.SetActive(false);
         frost.SetActive(true);
         naiden.SetActive(false);
         weazel.SetActive(false);
-        subs1.GetComponent<Text>().text = "Yeah, the rich bastards living there took everything they could and left us in this dump. Inderdaad, rijke klootzakken die daar wonen hebben alles voor hunzelf genomen en ons in de goot gelaten.";
-        yield return new WaitForSeconds(2f);
+        subs1.GetComponent<Text>().text = " rijke klootzakken die daar wonen hebben alles voor hunzelf genomen en ons in de goot gelaten.";
+        subs2.GetComponent<Text>().text = "Yeah, the rich bastards living there took everything they could and left us in this dump.";
+        yield return new WaitForSeconds(5f);
+
 
         aero.SetActive(false);
         beta.SetActive(false);
@@ -47,7 +62,8 @@ public class talk4_2 : MonoBehaviour
         naiden.SetActive(true);
         weazel.SetActive(false);
         subs1.GetComponent<Text>().text = "over klootzakken gesproken; ik zie een van batka’s mannen daar.";
-        yield return new WaitForSeconds(2f);
+        subs2.GetComponent<Text>().text = "Speaking of bastards; I see more of Batka’s men up ahead.";
+        yield return new WaitForSeconds(5f);
 
 
         aero.SetActive(false);
@@ -56,6 +72,7 @@ public class talk4_2 : MonoBehaviour
         naiden.SetActive(false);
         weazel.SetActive(false);
         subs1.GetComponent<Text>().text = "Jongens, aanvallen!";
+        subs2.GetComponent<Text>().text = "Let’s get em boys!";
         yield return new WaitForSeconds(2f);
 
 
